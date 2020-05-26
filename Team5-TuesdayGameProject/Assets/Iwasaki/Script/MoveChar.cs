93b449e8cdda1077d10e71b3107abd6c701fba5c
岩崎友哉 <tomoti2525@yahoo.co.jp>
2020-05-26 19:07:22 +0900

    次回始めやすくするためにフォルダ等を作成。

---
+++ Team5-TuesdayGameProject/Assets/Iwasaki/Script/MoveChar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChar : MonoBehaviour
+{
+    [SerializeField]
+    private float distance = 1;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, distance);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -distance);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-distance, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(distance, 0, 0);
+        }
+    }
+}
