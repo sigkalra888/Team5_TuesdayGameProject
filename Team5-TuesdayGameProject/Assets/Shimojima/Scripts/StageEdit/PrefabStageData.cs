60d11a1e25313ae1325074eb38a8efe1ba4a4dde
sigkalra <sigkalra8888@gmail.com>
2020-05-30 11:23:13 +0900

    [New]ステージエディットの作成(スクリプトを他のプロジェクトから持ってきたため検証作業開始)

---
+++ Team5-TuesdayGameProject/Assets/Shimojima/Scripts/StageEdit/PrefabStageData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(menuName = "PRefabStageData", fileName = "s")]
+public class PrefabStageData : ScriptableObject
+{
+    public string stageName;
+    [Tooltip("難易度")]
+    public int diificulty;
+    [Tooltip("アメフラシの数")]
+    public int amehurashiNum;
+    [Tooltip("水位が上がる量")]
+    public int increasedWaterVolume;
+    [Tooltip("ステージプレファブ")]
+    public GameObject stage;
+    [Tooltip("ステージセレクト用プレファブ")]
+    public GameObject viewStage;
+    [Tooltip("ステージデータ")]
+    public StageData sData;
+    [Tooltip("生成するグリッドの数")]
+    public Vector3Int gridCells;
+}
