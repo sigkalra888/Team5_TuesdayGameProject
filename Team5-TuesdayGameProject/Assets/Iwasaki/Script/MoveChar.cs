23baadba0bad02de094678417624b39dc44ea42b
岩崎友哉 <tomoti2525@yahoo.co.jp>
2020-06-02 18:51:00 +0900

    箱を押す処理を作成中。

--- Team5-TuesdayGameProject/Assets/Iwasaki/Script/MoveChar.cs
+++ Team5-TuesdayGameProject/Assets/Iwasaki/Script/MoveChar.cs
@@ -6,8 +6,22 @@ public class MoveChar : MonoBehaviour
 {
     [SerializeField]
     private float distance = 1;
+    private bool hittingBool = false;
+    [SerializeField]
+    private GameObject BOX;
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = BOX.GetComponent<Rigidbody>();
+    }
 
     void Update()
+    {
+        CharMove();
+        PushBox();
+    }
+    private void CharMove()
     {
         if (Input.GetKey(KeyCode.W))
         {
@@ -24,6 +38,31 @@ public class MoveChar : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(distance, 0, 0);
+        }
+    }
+
+    private void PushBox()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && hittingBool)
+        {
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Box")
+        {
+            hittingBool = true;
+            Debug.Log("true");
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Box")
+        {
+            hittingBool = false;
+            Debug.Log("false");
         }
     }
 }
