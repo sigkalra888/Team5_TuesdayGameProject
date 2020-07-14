70d831d3bf3a30b86eec7e13b87b911df6c49676
sigkalra <sigkalra8888@gmail.com>
2020-07-14 18:50:50 +0900

    [Update]レイヤーの追加、Boxの移動関数追加、その他

--- Team5-TuesdayGameProject/Assets/Shimojima/Scripts/StageObject/BoxController.cs
+++ Team5-TuesdayGameProject/Assets/Shimojima/Scripts/StageObject/BoxController.cs
@@ -20,10 +20,28 @@ public class BoxController : MonoBehaviour
     
     void Start()
     {
+        BoxParamInit();
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            PushBox(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            PushBox(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            PushBox(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            PushBox(3);
+        }
+
         RayCheack();
     }
 
@@ -32,13 +50,35 @@ public class BoxController : MonoBehaviour
         for (int i = 0; i < 4; i++)
         {
             pFlag[i].angleName = angleName[i];
-            pFlag[i].pushFlag = false;
+            pFlag[i].pushFlag = true;
         }
     }
 
+    /// <summary>
+    /// 指定方向に押す
+    /// </summary>
+    /// <param name="angleIndex"></param>
     public void PushBox(int angleIndex)
     {
-        
+        Debug.Log(angleIndex + ":" + pFlag[angleIndex].pushFlag);
+        if (!pFlag[angleIndex].pushFlag) { Debug.Log("これ以上押せない！"); return; }
+
+        switch (angleIndex) 
+        {
+            case 0:
+                transform.position += new Vector3(1, 0, 0);
+                break;
+            case 1:
+                transform.position += new Vector3(-1, 0, 0);
+                break;
+            case 2:
+                transform.position += new Vector3(0, 0, 1);
+                break;
+            case 3:
+                transform.position += new Vector3(0, 0, -1);
+                break;
+        }
+
     }
 
     /// <summary>
@@ -48,23 +88,45 @@ public class BoxController : MonoBehaviour
     {
         for (int i = 0; i < 4; i++)
         {
+            Vector3 rayStartPos = angleCollider[i].transform.position;
             Ray ray = new Ray();
+            RaycastHit hit;
+            int x = -1;
             switch (i)
             {
                 case 0:
-                    ray = new Ray(angleCollider[i].transform.position, new Vector3(-1, 0, 0));
+                    ray = new Ray(rayStartPos += new Vector3(0.1f, 0, 0), new Vector3(-1, 0, 0));
+                    x = 1;
                     break;
                 case 1:
-                    ray = new Ray(angleCollider[i].transform.position, new Vector3(1, 0, 0));
+                    ray = new Ray(rayStartPos += new Vector3(-0.1f, 0, 0), new Vector3(1, 0, 0));
+                    x = 0;
                     break;
                 case 2:
-                    ray = new Ray(angleCollider[i].transform.position, new Vector3(0, 0, 1));
+                    ray = new Ray(rayStartPos += new Vector3(0, 0, 0.1f), new Vector3(0, 0, -1));
+                    x = 3;
                     break;
                 case 3:
-                    ray = new Ray(angleCollider[i].transform.position, new Vector3(0, 0, -1));
+                    ray = new Ray(rayStartPos += new Vector3(0, 0, -0.1f), new Vector3(0, 0, 1));
+                    x = 2;
                     break;
             }
-            Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+            
+
+            if (Physics.Raycast(ray, out hit, 0.5f))
+            {
+                //非干渉ブロックに触れている方向に対応したpushFlagをfalseにする
+                if(hit.collider.gameObject.layer == 9)
+                {
+                    Debug.Log(hit.collider.gameObject.layer);
+                    if (!pFlag[x].pushFlag) { return; }
+                    Debug.Log("x:" + x);
+                    pFlag[x].pushFlag = false;
+                    Debug.Log(x + ":" + pFlag[x].pushFlag);
+                }
+            }
+            
+            Debug.DrawRay(ray.origin, ray.direction * 0.5f, Color.red);
         }
     }
 }
